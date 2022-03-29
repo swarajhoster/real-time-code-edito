efcaf7e0d3acf11ee5591a616401c07ee88c2b6d
Swaraj Kumar Singh <yuvrajsimasingh@gmail.com>
2022-03-29 17:58:18 +0530

    removed the port var

--- server.js
+++ server.js
@@ -63,5 +63,4 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+server.listen(5000, () => console.log(`Server Started`));
